e6cb7e78591d6121f0818bfcbf0c2f4a5d202adc
Enrico Favaro <enricofavaro@yahoo.it>
2021-01-29 16:49:51 +0000

    tic-tac-toe project

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,9 +1,14 @@
 def list_even(num_list):
 
+  even_numbers = []
+
   for num in num_list:
     if num % 2 == 0:
-      return True
+      even_numbers.append(num)
     else:
       pass
 
-list_even([2,4,6])
+  return even_numbers
+
+
+list_even([2,4,6,5,7])
