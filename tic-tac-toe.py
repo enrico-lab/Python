fd3bbcdda049940b7b841c9bf1e53067b5cc9f10
Enrico Favaro <enricofavaro@yahoo.it>
2021-01-30 19:00:36 +0000

    updating

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,19 @@
-#from Ipython.display import clear_output
+from IPython.display import clear_output
+clear_output()
+
+player1 = input("Please pick a marker 'X' or 'O'")
+
+position = int(input('Please enter a number'))
 
 def display_board(board):
   print(board[7]+'|'+board[8]+'|'+board[9])
-  print(' '+'|'+' '+'|'+' ')
   print('------')
   print(board[4]+'|'+board[5]+'|'+board[6])
   print('------')
   print(board[1]+'|'+board[2]+'|'+board[3])
 
 
-test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
+test_board = [' ']
+display_board('board')
+
+#def player_input():
