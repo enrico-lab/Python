2a181f4b662a0609da1c4e508b7d92842751dc3a
Enrico Favaro <enricofavaro@yahoo.it>
2021-02-02 11:57:06 +0000

    try/except

--- black_jack.py
+++ black_jack.py
@@ -71,6 +71,19 @@ class Chips:
 
 
 
+def take_bet(chips):
+
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
+
 
 
 
