e65b6547457c5291153d3b36759c4873a364477a
Enrico Favaro <enricofavaro@yahoo.it>
2021-02-04 19:02:41 +0000

    Volcanoes and population map with folium and pandas

--- mapping/map1.py
+++ mapping/map1.py
@@ -1,4 +1,47 @@
 import folium
+import pandas
 
- map = folium.Map(location=[51.54034781664162, -0.02100556927230834], zoom_start=6)
- map.save("Map1.html")
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+map = folium.Map(location=[51.54034781664162, -0.02100556927230834], zoom_start=6, tiles="Stamen Terrain")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el, nm in zip(lat,lon,elev,name):
+    iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m",
+     fill_color=color_producer(el), color = "grey", fill_opacity = 0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+  style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000
+  else 'blu' if 10000000 <= x['properties']['POP2005'] < 40000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
+
+map.save("Map_html_popup_advanced.html")
+
