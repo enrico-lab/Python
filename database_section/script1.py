63439ab46ebaa5b09f13141f7334a5a05c736a89
Enrico Favaro <enricofavaro@yahoo.it>
2021-02-07 17:37:17 +0000

    postgress database

--- database_section/script1.py
+++ database_section/script1.py
@@ -2,16 +2,16 @@ import psycopg2
 
 
 def create_table():
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect(" dbname='database1' user='postgres' password='europe+2' host='localhost' port='5433' ")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item,quantity,price):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='' user='' password='' host='localhost' port=''")
     cur=conn.cursor()
-    cur.execute("INSERT INTO store VALUES (?,?,?)",(item,quantity,price))
+    cur.execute("INSERT INTO store VALUES (%s,%s,%s)",(item,quantity,price))
     conn.commit()
     conn.close()
 
@@ -19,7 +19,7 @@ def insert(item,quantity,price):
 
 
 def view():
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='' password='' host='localhost' port=''")
     cur=conn.cursor()
     cur.execute("SELECT * FROM store")
     rows=cur.fetchall()
@@ -27,19 +27,22 @@ def view():
     return rows
 
 def delete(item):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='' password='' host='localhost' port=''")
     cur=conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='' password='' host='localhost' port=''")
     cur=conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    cur.execute("UPDATE store SET quantity=%s, price=%s WHERE item=%s",(quantity,price,item))
     conn.commit()
     conn.close()
 
-update(11,6,'Water Glass')
-# delete("Coffee cup")
+create_table()
+# insert("Apple",10,5)
+# insert("Orange",15,10)
+update(11,6,'Apple')
+#delete("Orange")
 print(view())
