0aa080d1b9fc5754b5e905572bf0c06f410629ba
Enrico Favaro <enricofavaro@yahoo.it>
2021-02-08 17:51:27 +0000

    password generator completed

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,9 +1,50 @@
 from tkinter import *
-
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    capitals = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    password_letters = [choice(letters) for _ in range(randint(4, 6))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_capitals = [choice(capitals) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers + password_capitals
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    e_password.insert(0, password)
+    pyperclip.copy(password)
+
+
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = e_website.get()
+    email = e_email.get()
+    password = e_password.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Opss", message="Please make sure you haven't left any field empty.")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details you entered: \nEmail: {email}"
+                                                   f"\nPassword: {password}\n Is it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                e_website.delete(0, END)
+                e_password.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -34,9 +75,9 @@ e_email.insert(0, "enricofavaro@yahoo.it")
 e_password = Entry(width=21)
 e_password.grid(row=3, column=1)
 
-b_password = Button(text="Generate Paasword", width=10)
+b_password = Button(text="Generate Password", width=10, command=generate_password)
 b_password.grid(row=3, column=2)
-add_button = Button(text="ADD", width=33)
+add_button = Button(text="ADD", width=33, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
